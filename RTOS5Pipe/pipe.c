91f21170009268816b8c699b7657188aff7d7582
VijaySanthoshAkula <32711452+VijaySanthoshAkula@users.noreply.github.com>
2018-02-14 00:28:48 +0530

    RTOS Assignment related to Pipes

---
+++ RTOS5Pipe/pipe.c
@@ -0,0 +1,44 @@
+
+
+
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+        int des_p[2];
+        if(pipe(des_p) == -1) {
+          perror("Pipe failed");
+          exit(1);
+        }
+
+        if(fork() == 0)            //first fork
+        {
+            close(STDOUT_FILENO);  //closing stdout
+            dup(des_p[1]);         //replacing stdout with pipe write 
+            close(des_p[0]);       //closing pipe read
+            system("ls -1 *.txt");
+            
+          //  exit(0);
+        }
+
+        else            
+        {
+            wait(0);
+            close(STDIN_FILENO);   //closing stdin
+            dup(des_p[0]);         //replacing stdin with pipe read
+            close(des_p[1]);       //closing pipe write
+            system("wc -l");
+           // exit(0);
+        }
+
+        
+       
+        //wait(0);
+        close(des_p[0]);
+        close(des_p[1]);
+        return 0;
+}
+
+
